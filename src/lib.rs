585494e6d00922950a278107b3edcf2c191304de
张实唯 <ylxdzsw@gmail.com>
2021-10-21 14:13:11 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,4 @@ pub type ContMut<'a, T, S> = &'a mut dyn Fn(T) -> S;
 
 // TODO: inlining closures? it may also simplify the yin yang example
 // TODO: the simple stmt-based non-recursive apporach cannot handle shifts that inside control flows (loop { shift(...) })
-//       specialize if and while and make them work?
+//       A better way is to reimplement using generators. It can also be multi-shot when https://github.com/rust-lang/rust/issues/57972 lands.
